b35cd8b43fa491b377df795e201c14561a695df4
Elie Wolfe <elupus@gmail.com>
2025-02-19 15:50:30 -0500

    Use inflation as submodule (so it does not have to be locally installed)

--- inflation_package.py
+++ inflation_package.py
@@ -1,4 +1,4 @@
-from inflation import InflationProblem, InflationLP
+from inflation.inflation import InflationProblem, InflationLP
 import numpy as np
 
 triangle = InflationProblem(dag={"rho_AB": ["A", "B"],
@@ -16,6 +16,10 @@ for i in range(3):
     prob_disagree[i, (i+1)%3, (i+2)%3,0,0,0] = 1/6
     prob_disagree[i, (i+2)%3, (i+1)%3,0,0,0] = 1/6
     
-InfLP.set_distribution(prob_disagree, use_lpi_constraints=True)
+InfLP.set_distribution(prob_disagree, use_lpi_constraints=False)
 InfLP.solve()
+for k, v in InfLP.certificate_as_dict().items():
+    if k.n_factors == 3:
+        print(f"{k} : {v}")
+print(InfLP.certificate_as_string())
 
